520a7e3a5435bf6283dc97fb7021c9653a1bf310
Kiattirat S <frankent@gmail.com>
2023-01-24 16:54:52 +0700

    fix from fang

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
-const withTM = require("next-transpile-modules")(["@meetperry/amity-uikit"]);
+// const withTM = require("next-transpile-modules")(["@meetperry/amity-uikit"]);
 const nextConfig = {
   reactStrictMode: true,
 };
 
-module.exports = withTM(nextConfig);
+// module.exports = withTM(nextConfig);
+
+module.exports = nextConfig;
